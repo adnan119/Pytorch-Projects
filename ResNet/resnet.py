da2550e6d716ae574c04819ae497705f5d5fe0ec
Adnan Pen <adnanpen119@gmail.com>
2021-09-15 20:25:23 +0530

    Update resnet.py

--- ResNet/resnet.py
+++ ResNet/resnet.py
@@ -1,21 +1,25 @@
 import torch
 import torch.nn as nn
 
+
 class block(nn.Module):
-    def __init__(self,in_channels, out_channels, identity_downsample=None, stride=1):
-        super().__init__()
+    def __init__(
+      self,in_channels, out_channels, identity_downsample=None, stride=1
+      ):
+        super(block, self).__init__()
         self.expansion = 4
         self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1, padding=0)
         self.bn1 = nn.BatchNorm2d(out_channels)
-        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=stride, padding=1)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias= False)
         self.bn2 = nn.BatchNorm2d(out_channels)
-        self.conv3 = nn.Conv2d(out_channels, out_channels*self.expansion, kernel_size=1, stride=1, padding=0)
+        self.conv3 = nn.Conv2d(out_channels, out_channels * self.expansion, kernel_size=1, stride=1, padding=0, bias=False)
         self.bn3 = nn.BatchNorm2d(out_channels * self.expansion)
         self.relu = nn.ReLU()
         self.identity_downsample = identity_downsample
+        self.stride = stride
 
     def forward(self, x):
-        identity = x
+        identity = x.clone()
 
         x = self.conv1(x)
         x = self.bn1(x)
@@ -38,7 +42,7 @@ class ResNet(nn.Module): #ResNet50 blocks: [3, 4, 6, 3]
     def __init__(self, block, layers, image_channels, num_classes):
         super(ResNet, self).__init__()
         self.in_channels = 64
-        self.conv1 = nn.Conv2d(image_channels, 64, kernel_size=7, stride=2, padding=3)
+        self.conv1 = nn.Conv2d(image_channels, 64, kernel_size=7, stride=2, padding=3, bias=False)
         self.bn1 = nn.BatchNorm2d(64)
         self.relu = nn.ReLU()
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
@@ -67,14 +71,21 @@ class ResNet(nn.Module): #ResNet50 blocks: [3, 4, 6, 3]
         x = x.reshape(x.shape[0], -1)
         x = self.fc(x)
 
+        return x
+
     def _make_layer(self, block, num_blocks, out_channels, stride):
         identity_downsample = None
         layers = []
 
         if stride != 1 or self.in_channels != out_channels * 4:
-            identity_downsample = nn.Sequential(nn.Conv2d(self.in_channels, out_channels*4, kernel_size=1,
-             stride=stride),
-             nn.BatchNorm2d(out_channels*4))
+            identity_downsample = nn.Sequential(
+              nn.Conv2d(
+                self.in_channels,
+                out_channels*4,
+                kernel_size=1,
+                stride=stride
+              ),
+                nn.BatchNorm2d(out_channels*4))
 
         layers.append(block(self.in_channels, out_channels, identity_downsample, stride))
         self.in_channels = out_channels * 4
@@ -97,7 +108,6 @@ def ResNet152(image_channels=3, num_classes=1000):
     return ResNet(block, [3,8,36,3], image_channels, num_classes)
 
 def test():
-    net = ResNet152()
-    x = torch.randn(2, 3, 224, 224)
-    y = net(x).cuda()
-    print(y.shape)
+    net = ResNet101(image_channels=3, num_classes=1000)
+    y = net(torch.randn(4, 3, 224, 224)).to("cuda")
+    print(y.size())
